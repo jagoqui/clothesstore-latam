6a55ac03b0912dde6af0676059da8f4874366398
Jaidiver Gómez Quintero <jaidiver.gomez@udea.edu.co>
2021-08-26 17:49:52 -0500

    Se inicia a mejorar los estilos responsive de la app.

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,9 +6,20 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  @Input() sidebarMode: boolean = false;
+  @Output() sidebarClose: EventEmitter<boolean> = new EventEmitter<boolean>(false);
   @Output() query: EventEmitter<String> = new EventEmitter<String>();
 
+  globalListenFunc: { (): void; (): void };
+
+  constructor(private renderer: Renderer2) {
+    this.globalListenFunc = this.renderer.listen('document', 'keydown.escape', () => {
+      this.sidebarClose.emit(true);
+    });
+  }
+
   onFilter(query: string) {
+    this.sidebarClose.emit(true);
     this.query.emit(query);
   }
 }
