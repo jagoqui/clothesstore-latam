f4ad982599468953b94174b27b8dba307effa7c9
Jaidiver Gómez Quintero <jaidiver.gomez@udea.edu.co>
2021-09-08 17:08:50 -0500

    Se agrega webpack bundle analizer, para detectar problemas de optimización de la app.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { LottieModule } from 'ngx-lottie';
 import { HeaderComponent } from '@appShared/components/header/header.component';
 import { SearchComponent } from '@appShared/components/search/search.component';
 import { FooterComponent } from '@appShared/components/footer/footer.component';
@@ -9,10 +8,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
-export const playerFactory = () => import(/* webpackChunkName: 'lottie-web' */ 'lottie-web');
-
 const componentsExport = [HeaderComponent, FooterComponent];
-const modulesExport = [CommonModule, RouterModule, LottieModule.forRoot({ player: playerFactory })];
+const modulesExport = [CommonModule, RouterModule];
 
 @NgModule({
   declarations: [componentsExport, SearchComponent, NavbarComponent],
